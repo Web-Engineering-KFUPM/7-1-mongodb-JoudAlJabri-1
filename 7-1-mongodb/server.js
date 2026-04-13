11c6597a4375da2e0dfe38667e013af4c08b5c0e
Joud Aljabri <s202277140@kfupm.edu.sa>
2026-04-13 15:20:06 +0300

    ToDo6

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -222,3 +222,8 @@ async function updateStudent() {
       updateStudent();
 
 // delete document
+async function deleteStudent() {
+         await Student.deleteOne({ name: "Sara" });
+      console.log("✅ Deleted Sara");
+      }
+      deleteStudent();
