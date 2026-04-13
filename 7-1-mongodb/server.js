946477a0a05c87da57fd74222d1172cefa717a4d
Joud Aljabri <s202277140@kfupm.edu.sa>
2026-04-13 19:57:02 +0300

    ToDo1-Fix

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -188,7 +188,7 @@
 import mongoose from "mongoose";
 
 // establish connection
-mongoose.connect("mongodb+srv://joudjabri_db_user:JiiJii&&886@cluster0.h8285ts.mongodb.net/");
+mongoose.connect("mongodb+srv://joudjabri_db_user:JiiJii&&886@cluster0.h8285ts.mongodb.net/TestDB");
 // define schema
 const studentSchema = new mongoose.Schema({
          name: String,
